5ed6bc4d58ef4be26aa4faaa331ada5110d3a2ad
Himanshu Raikwar <46955050+iamhimanshuraikwar@users.noreply.github.com>
2024-08-10 13:37:34 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import React from 'react';
 import { Calendar, Flag, Trash2, Archive } from 'lucide-react';
 
 const priorities = {
@@ -141,10 +142,19 @@ const TodoList = () => {
             </li>
           ))}
         </ul>
-      </div>
-      <div className="fixed bottom-0 left-0 right-0 bg-white py-2 text-center text-sm text-gray-500 border-t">
-        Created by <a href="https://himanshuraikwar.com/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Himanshu Raikwar</a>
-      </div>
+      const Footer = () => (
+  <div className="fixed bottom-0 left-0 right-0 bg-gray-800 py-2 text-center text-sm text-white border-t flex items-center justify-center">
+    <span className="mr-2">Made with</span>
+    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-red-500 mr-2" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
+    </svg>
+    <span>by</span>
+    <a href="https://himanshuraikwar.com/" target="_blank" rel="noopener noreferrer" className="text-blue-300 hover:underline ml-1">
+      Himanshu Raikwar
+    </a>
+  </div>
+);
+
     </div>
   );
 };
